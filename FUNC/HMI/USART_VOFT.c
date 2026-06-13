34e7a9463b1875fb5e13b0a18378e1f9eb1fc3d0
yaomin20 <2036401564@qq.com>
2026-06-13 14:12:12 +0800

    提交代码

--- FUNC/HMI/USART_VOFT.c
+++ FUNC/HMI/USART_VOFT.c
@@ -2,7 +2,7 @@
 #include "func_main.h"
 
 
-// ʹ��ʱ��ʼ��
+// 濞达綀娉曢弫銈夊籍鐠哄搫鐏ュ┑顔碱儏鐎碉拷
 struct Frame FrameV = {
     .fdata = {0.0f},
 	.data = {0,0,0,0},
@@ -13,18 +13,18 @@ static int append_array_fixed(uint8_t* c,
                        const uint8_t* a, size_t a_len, 
                        const uint8_t* b, size_t b_len) 
 {
-    // �������
+    // 闁告瑥鍊归弳鐔肺涢埀顒勫蓟閿燂拷
     if (c == NULL || a == NULL || b == NULL) {
         return -1;
     }
     
-    // ���� a ����
+    // 濠㈣泛绉撮崺锟� a 闁轰焦澹嗙划锟�
     memcpy(c, a, a_len * sizeof(uint8_t));
     
-    // ���� b ���鵽 a ����
+    // 濠㈣泛绉撮崺锟� b 闁轰焦澹嗙划宥夊礆閿燂拷 a 闁告艾閰ｅ锟�
     memcpy(c + a_len, b, b_len * sizeof(uint8_t));
     
-    return 0; // �ɹ�
+    return 0; // 闁瑰瓨鍔曟慨锟�
 }
 
 
@@ -42,15 +42,15 @@ void VOFT_comClark(void)
 
 void VOFT_SendData(void)
 {
-    //���ݺϲ�����������buf
+    //闁轰胶澧楀畵渚€宕ラ崼婵婂珯闁归鏌夌粈澶愬礆閻楀牊娈堕柟鍦暁uf
 	memcpy(FrameV.data,(uint8_t*)&FrameV.fdata,sizeof(FrameV.fdata));
 	
 	size_t a_len = CH_COUNT*4;
     size_t b_len = 4;
     size_t total_len = a_len + b_len;    
-    // �����������
+    // 闁告帒妫濋崢銈嗘綇閹惧啿姣夐柡浣瑰缁拷
     uint8_t c[total_len];
-    //��������ͷ
+    //闁告梻濮崇粭鍌炲极閻楀牆绁﹀璁规嫹
     int ret = append_array_fixed(c, FrameV.data, a_len, FrameV.tail, b_len);
 	if (ret == 0) 
 	{
@@ -95,31 +95,31 @@ void VOFT_sendPWM(void)
 
 void VOFT_sendUdc(void)
 {
-//	//n�����ݵ�����ȡ
+//	//n濞戞搩浜濋弳鐔煎箲椤旂厧绀嬮柣娆樺墲楠炲繘宕ｉ敓锟�
 //	uint8_t bufsize = 0;
 //	HAL_ADC_Start(&hadc1);
 //	temp[bufsize++] = HAL_ADC_GetValue(&hadc1);
 //	temp[bufsize++] = 1;
-//	//���ݺϲ�����������buf
+//	//闁轰胶澧楀畵渚€宕ラ崼婵婂珯闁归鏌夌粈澶愬礆閻楀牊娈堕柟鍦暁uf
 //	memcpy(FrameV.data,(uint8_t*)&temp,sizeof(temp));
 
   HAL_ADCEx_InjectedStart_IT(&hadc1);
   HAL_ADCEx_InjectedStart_IT(&hadc2);
 	
-	//n�����ݵ�����ȡ
+	//n濞戞搩浜濋弳鐔煎箲椤旂厧绀嬮柣娆樺墲楠炲繘宕ｉ敓锟�
 	uint8_t Xbufsize = 0;
 	HAL_ADC_Start(&hadc1);
-	FrameV.fdata[Xbufsize++] = HAL_ADC_GetValue(&hadc1); //��λ��          //����ʽ��ȡ�������Ż�
+	FrameV.fdata[Xbufsize++] = HAL_ADC_GetValue(&hadc1); //闁汇垹鍚€缂嶅懘宕抽敓锟�          //闂傚啳顕ч、锝咁嚕韫囨凹鍤㈤柛娆愮壄缁辨繈宕ユ惔锝囨暰濞村吋锚鐎碉拷
 	
 	HAL_ADC_Start(&hadc2);
-	FrameV.fdata[Xbufsize++] = HAL_ADC_GetValue(&hadc2)*0.02094726f; //ĸ�ߵ�ѹ 
-	//4096��Ӧ3.3V
-	//x��Ӧ3.3*x/4096V*78/3  V
+	FrameV.fdata[Xbufsize++] = HAL_ADC_GetValue(&hadc2)*0.02094726f; //婵絽绉堕崵搴ㄦ偨闂堟稑绔� 
+	//4096閻庣數鎳撶花锟�3.3V
+	//x閻庣數鎳撶花锟�3.3*x/4096V*78/3  V
 	
-	//ʹ��Q15����
-	//0.02094726ʹ��// ��� result ������������ Q15 ��������ʾ
+	//濞达綀娉曢弫顥�15閻庤姘ㄩ崑锟�
+	//0.02094726濞达綀娉曢弫锟�// 缂備焦鎸婚悘锟� result 閻忓繗椴稿Σ鎼佹偋閳哄啯鍊為梺鎻掔箳濞堬拷 Q15 閻庤姘ㄩ崑锝夊极閹峰被鈧啰绮堥敓锟�
 	//int32_t result = ((int32_t)x * 686) >> 15;
-	//FrameV.fdata[Xbufsize++] = (HAL_ADC_GetValue(&hadc2)*686)>>15; //ĸ�ߵ�ѹ 
+	//FrameV.fdata[Xbufsize++] = (HAL_ADC_GetValue(&hadc2)*686)>>15; //婵絽绉堕崵搴ㄦ偨闂堟稑绔� 
 	
 	
 	
